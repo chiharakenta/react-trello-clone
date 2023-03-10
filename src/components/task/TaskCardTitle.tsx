955c10d2eb006adf9c020ee0adaf24e2e1c58e05
chiharakenta <chiharakenta0519@gmail.com>
2023-03-10 16:06:00 +0900

    feat: TaskCardTitle

--- src/components/task/TaskCardTitle.tsx
+++ src/components/task/TaskCardTitle.tsx
@@ -1,5 +1,50 @@
-import { FC } from 'react';
+import {
+  ChangeEventHandler,
+  FC,
+  FocusEventHandler,
+  FormEventHandler,
+  MouseEventHandler,
+  useState
+} from 'react';
 
 export const TaskCardTitle: FC = () => {
-  return <div></div>;
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState('Today');
+
+  const handleClick: MouseEventHandler<HTMLDivElement> = () => {
+    setIsClick(true);
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputCardTitle(event.target.value);
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    setIsClick(false);
+  };
+
+  const handleBlur: FocusEventHandler<HTMLInputElement> = () => {
+    setIsClick(false);
+  };
+
+  return (
+    <div onClick={handleClick} className="taskCardTitleInputArea">
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            className="taskCardTitleInput"
+            autoFocus
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={inputCardTitle}
+            maxLength={10}
+          />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
 };
