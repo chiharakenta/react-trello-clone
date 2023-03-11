e92d61c2dc7ddeb67676dace2b9da557351adb3f
chiharakenta <chiharakenta0519@gmail.com>
2023-03-11 17:43:50 +0900

    style: task trash button icon

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,20 +1,25 @@
 import { css } from '@emotion/react';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FC } from 'react';
 import { TaskType } from './Task.type';
 
+const styles = {
+  taskBox: css``,
+  taskText: css``,
+  taskTrashButton: css``
+};
 interface Props {
   task: TaskType;
 }
-
-const styles = {
-  taskBox: css``,
-  taskText: css``
-};
 
 export const Task: FC<Props> = ({ task }) => {
   return (
     <div css={styles.taskBox}>
       <p css={styles.taskText}>{task.text}</p>
+      <button css={styles.taskTrashButton}>
+        <FontAwesomeIcon icon={faTrashCan} />
+      </button>
     </div>
   );
 };
