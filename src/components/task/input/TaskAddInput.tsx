b0ae36d5e296a206509bd0fad52bb8b1505dfd86
chiharakenta <chiharakenta0519@gmail.com>
2023-03-11 16:55:24 +0900

    style: TaskAddInput

--- src/components/task/input/TaskAddInput.tsx
+++ src/components/task/input/TaskAddInput.tsx
@@ -1,5 +1,27 @@
-import { FC } from 'react';
+import { css } from '@emotion/react';
+import { FC, FormEventHandler } from 'react';
+
+const styles = {
+  taskAddInput: css`
+    width: 100%;
+    font-size: 1.3rem;
+    padding: 10px 15px;
+    border-radius: 3px;
+    border: none;
+    outline: none;
+  `
+};
 
 export const TaskAddInput: FC = () => {
-  return <div></div>;
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    console.log(event);
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input css={styles.taskAddInput} type="text" placeholder="add a task" />
+      </form>
+    </div>
+  );
 };
