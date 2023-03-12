c556c58a7190d81668c548e797176594a10222bd
chiharakenta <chiharakenta0519@gmail.com>
2023-03-12 13:55:39 +0900

    style: TaskCards & AddTaskCardButton

--- src/components/task/TaskCards.tsx
+++ src/components/task/TaskCards.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
 import { TaskCard } from './TaskCard';
 import { AddTaskCardButton } from './button/AddTaskCardButton';
+import { css } from '@emotion/react';
+
+const styles = {
+  taskCardsArea: css`
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-wrap: wrap;
+  `
+};
 
 export const TaskCards: FC = () => {
   return (
-    <div>
+    <div css={styles.taskCardsArea}>
       <TaskCard />
       <AddTaskCardButton />
     </div>
--- src/components/task/button/AddTaskCardButton.tsx
+++ src/components/task/button/AddTaskCardButton.tsx
@@ -1,5 +1,30 @@
+import { css } from '@emotion/react';
 import { FC } from 'react';
 
+const styles = {
+  addTaskCardButtonArea: css`
+    margin-left: 1%;
+    margin-top: 25px;
+  `,
+  addTaskCardButton: css`
+    padding: 10px 18px;
+    border-radius: 9999px;
+    font-size: 30px;
+    background-color: rgb(221, 194, 162);
+    box-shadow: 3px 3px 6px 1px #000;
+    cursor: pointer;
+    transition: all 0.3s;
+    &:hover {
+      box-shadow: none;
+      transform: translate(3px, 3px);
+    }
+  `
+};
+
 export const AddTaskCardButton: FC = () => {
-  return <div></div>;
+  return (
+    <div css={styles.addTaskCardButtonArea}>
+      <button css={styles.addTaskCardButton}>+</button>
+    </div>
+  );
 };
